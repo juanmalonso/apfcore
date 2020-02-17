5c573cc7dfac009aa41bcacbb27a9d122580d592
juanmalonso <manialonso@gmail.com>
2020-02-17 11:29:50 -0300

    adaptacion nubesys nbs40

---
+++ src/Auth/Ui/Pages/Login.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Nubesys\Auth\Ui\Pages;
+
+use Nubesys\Vue\Services\VueUiService;
+
+//COMPONENTS
+use Nubesys\Core\Ui\Components\Basic\Button;
+
+class Login extends VueUiService {
+
+    public function mainAction($p_params){
+
+        $this->setTitle("Titulo de Ejemplo la Pagina");
+
+        
+        $this->setJsDataVar("test","testvalue");
+
+        $button = new Button($this->getDI());
+        $this->placeComponent("content", $button, $p_params);
+        
+    }
+}
