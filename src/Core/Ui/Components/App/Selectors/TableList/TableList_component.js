d03a44cbcefda237229539e9d323433386075324
juanmalonso <manialonso@gmail.com>
2020-03-16 17:55:18 -0300

    se agrego calculo de columnas para cospan de paginador

--- src/Core/Ui/Components/App/Selectors/TableList/TableList_component.js
+++ src/Core/Ui/Components/App/Selectors/TableList/TableList_component.js
@@ -8,9 +8,23 @@ Vue.component("___tag_", {
 
     console.log(_.size(this.linksFields), this.linksFields);
   },
-  methods: {
-  },
+  methods: {},
   computed: {
+    colsNum: function() {
+      var result = 1; //numrows column
+
+      result += _.size(this.tableFields);
+
+      if (_.size(this.linksFields) > 0) {
+        result += 1;
+      }
+
+      if (_.size(this.actionsFields) > 0) {
+        result += 1;
+      }
+
+      return result;
+    }
   },
   template: "#___tag_-template"
 });
